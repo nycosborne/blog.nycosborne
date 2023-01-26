c2fd527dcc759eb330898b68d7fcda01ce8f96ad
dano <nycosborne@gmail.com>
2023-01-27 00:14:38 +0300

    PostController.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'summary',
+        'content',
+    ];
+
     use HasFactory;
 }
