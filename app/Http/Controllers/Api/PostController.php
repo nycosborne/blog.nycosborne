4079b1b6e30c3db6699f4716fe2899d7cb150f29
dano <nycosborne@gmail.com>
2023-01-31 16:30:53 -0500

    Load with was work around but I need to get a handle on resources and collection

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
     public function index()
     {
 //        return PostResource::collection(Post::query()->orderBy('id', 'desc')->paginate(10));
-        return PostResource::collection(Post::with('category')->get());
+        return PostResource::collection(Post::all());
 //        return PostResource::collection(Post::with('category')->first());
 //clock(json_encode(Post::with('category')->get()));
 
