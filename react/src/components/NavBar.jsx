6c4e4d94bead669c5551b603e4ac954356ddfa17
dano <nycosborne@gmail.com>
2023-07-21 21:34:13 +0300

    handling case where post has no image

--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -1,12 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {useStateContext} from "../context/ContextProvider.jsx";
+import { useStateContext } from "../context/ContextProvider.jsx";
 import axiosClient from "../axios-client.js";
 
 function NavBar() {
-
-    const {user, setUser, setToken, isAdmin} = useStateContext();
+    const { user, setUser, setToken, isAdmin } = useStateContext();
 
     const styles = {
         container: {
@@ -15,9 +14,7 @@ function NavBar() {
         },
         navBar: {
             background: '#1a202c',
-            // overflowX: '100%',
             color: '#7f8ea3',
-            // color: '#7f8ea3',
         }
     }
 
@@ -31,27 +28,29 @@ function NavBar() {
     }
 
     return (
-        <Navbar variant="dark" style={styles.navBar}>
+        <Navbar variant="dark" style={styles.navBar} expand="md">
             <Container style={styles.container}>
-                <Navbar.Brand href="/">nycosborne's web_Log </Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="/posts">all</Nav.Link>
-                    <Nav.Link href="/tags">tags</Nav.Link>
-                    {/*categories are all set up if I ever need to implement*/}
-                    {/*<Nav.Link href="/categories">categories</Nav.Link>*/}
-                </Nav>
-                <Nav className="justify-content-end">
-                    <Nav.Link target="_blank" href="https://github.com/nycosborne">GitHub</Nav.Link>
-                    <Nav.Link target="_blank" href="https://twitter.com/nycosborne">Twitter</Nav.Link>
+                <Navbar.Brand href="/">nycosborne's web_Log</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/posts">all</Nav.Link>
+                        <Nav.Link href="/tags">tags</Nav.Link>
+                        {/*categories are all set up if I ever need to implement*/}
+                        {/*<Nav.Link href="/categories">categories</Nav.Link>*/}
+                    </Nav>
+                    <Nav className="justify-content-end">
+                        <Nav.Link target="_blank" href="https://github.com/nycosborne">GitHub</Nav.Link>
+                        <Nav.Link target="_blank" href="https://twitter.com/nycosborne">Twitter</Nav.Link>
 
-                {
-                    isAdmin &&
-                    <>
-                    <Nav.Link href="/dashboard">Admin</Nav.Link>
-                    <Nav.Link href="#" onClick={onLogout}> {user.name} Log Out</Nav.Link>
-                    </>
-                }
-                </Nav>
+                        {isAdmin &&
+                            <>
+                                <Nav.Link href="/dashboard">Admin</Nav.Link>
+                                <Nav.Link href="#" onClick={onLogout}> {user.name} Log Out</Nav.Link>
+                            </>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
